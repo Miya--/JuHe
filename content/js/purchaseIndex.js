0ca3d63ba1ad29a2f643c276284aaccf8060a0c9
Miya <yjhr1126@sina.com>
2015-06-03 08:27:23 +0800

    订单确认页面。

--- content/js/purchaseIndex.js
+++ content/js/purchaseIndex.js
@@ -1,43 +1,32 @@
 $(function(){
-    // 查询条件中的“更多”按钮
-    $('#getTypeMore').click(function(){
-        var selObj = $(this);
-        if(selObj.attr('status') == 'down'){
-            $('#typeList').find('li[isover="1"]').css('display', 'block');
-            selObj.attr('status', 'up').html('收起 <img src="content/images/arrow-up1.png" />');
-        }else{
-            $('#typeList').find('li[isover="1"]').css('display', 'none');
-            selObj.attr('status', 'down').html('更多 <img src="content/images/arrow-down1.png" />');
-        }
-        
-    });
-
     // 下拉框初始化
     $('#selectPro').select();
     $('#selectCity').select();
     $('#selectQu').select();
 
+    var isShowBtnOption = false;
     $('.label_check').click(function(){
-        if($('.sear-condit').find('.c_on').length > 0){
-            $('#btnOption').css('display', 'block');
-        }else{
-            $('#btnOption').css('display', 'none');
+        if(!isShowBtnOption){
+            if($('.sear-condit').find('.c_on').length > 0){
+                $('#btnOption').css('display', 'block');
+            }else{
+                $('#btnOption').css('display', 'none');
+            }
         }
     });
 
     $('.combo-box').click(function(){
-        var isSelect = false;
-        $('select').each(function(){
-            alert($(this).val());
-            if($(this).val() != ''){
-                isSelect == true;
+        isShowBtnOption = false;
+        $('select').each(function(i){
+            var obj = $('select').eq(i);
+            if(obj.val() != ''){
+                isShowBtnOption = true;
             }
         });
-        alert(isSelect);
-        if(isSelect){
+        if(isShowBtnOption || $('.sear-condit').find('.c_on').length > 0){
             $('#btnOption').css('display', 'block');
         }else{
-            $('#btnOption').css('display', 'none');
+            $('#btnOption').css('display', 'none'); 
         }
     });
 });
